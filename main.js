d75a3a42cf41f003ea461c8b5a8848985d0f58f2
Fenn <dawson.june@outlook.com>
2022-02-18 01:53:35 -0800

    Added more styling and customization

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 let sketchBox = document.querySelector(".sketch-container");
 let resetButton = document.querySelector(".reset-btn");
-let sketchSize = 800;
 
 /* Event Listeners */
 
@@ -12,9 +11,36 @@ resetButton.addEventListener('click', () => {
 
 /* Main Code */
 
-addDivs(16);
+playGame();
 
-function addDivs(divNum) {
+function playGame() {
+    let sketchSize = setSketchSize();
+    let divNums = promptForGrid();
+    addDivs(divNums, sketchSize);
+}
+
+function setSketchSize() {
+    let newSketchSize = +prompt("What size should the sketch body be? Must be larger than 300px but smaller than 701px.");
+    if(newSketchSize != undefined && !isNaN(newSketchSize) && Number.isInteger(newSketchSize)) {
+        if(newSketchSize % 2 != 0 || newSketchSize % 5 != 0) {
+            alert("Must be an even number or divisible by 5!");
+            setSketchSize();
+        }
+        else if(newSketchSize > 700) {
+            alert("Must be smaller than 700px.");
+            setSketchSize();
+        }
+        else if(newSketchSize < 399) {
+            alert("Must be larger than 400.");
+            setSketchSize();
+        }
+        else {
+            return newSketchSize;
+        }
+    }
+}
+
+function addDivs(divNum, sketchSize) {
     let counter = 0;
     let sketchStyle = sketchBox.style;
     sketchStyle.height = `${sketchSize}px`;
@@ -30,13 +56,15 @@ function addDivs(divNum) {
         let divStyle = newDiv.style;
         divStyle.height = `${divSize}px`;
         divStyle.width = `${divSize}px`;
+        divStyle.backgroundColor = 'Black';
+        divStyle.opacity = 0;
         sketchBox.appendChild(newDiv);
     } while(counter < (divNum * divNum));
 }
 
 function hoverChange(divToChange) {
-    let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    divToChange.style.backgroundColor = randomColor;
+    let newOpacity = +divToChange.style.opacity + 0.1;
+    divToChange.style.opacity = newOpacity;
 }
 
 function resetSketch() {
@@ -44,25 +72,25 @@ function resetSketch() {
     addedDivs.forEach(selectedDiv => {
         selectedDiv.remove();
     });
-    promptForNewGrid();
+    promptForGrid();
 }
 
-function promptForNewGrid() {
-    let gridDivNums = +prompt("How many squares per side of the new grid? Must be less than 100.");
+function promptForGrid() {
+    let gridDivNums = +prompt("How many squares per side of the grid? Must be less than 100.");
     if(gridDivNums != undefined && !isNaN(gridDivNums) && Number.isInteger(gridDivNums)) {
         if(gridDivNums <= 0) {
             alert("Must be greater than 0.");
-            promptForNewGrid();
+            promptForGrid();
         }
         else if(gridDivNums >= 100) {
             alert("Must be less than 100.");
-            promptForNewGrid();
+            promptForGrid();
         }
         else
-            addDivs(gridDivNums);
+            return gridDivNums;
     }
     else {
         alert("Must be a valid number.");
-        promptForNewGrid();
+        promptForGrid();
     }
 }
