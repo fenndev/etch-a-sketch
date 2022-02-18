aad98c8741d2876718fe2a8e14e572783afa5722
Fenn <dawson.june@outlook.com>
2022-02-17 20:40:26 -0800

    Perfected grid generation

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 /* Global Variables */ 
 
-let containerBox = document.querySelector(".container");
+let sketchBox = document.querySelector(".sketch-container");
 let sketchSize = 960;
 
 /* Event Listeners */
 
 /* Main Code */
 
+function onLoad() {
+    
+}
+
 function addDivs(divNum) {
     let counter = 0;
-    let divSize = sketchSize / divNum;
+    let sketchStyle = sketchBox.style;
+    sketchStyle.height = `${sketchSize}px`;
+    sketchStyle.width = `${sketchSize}px`;
+    let divSize = ((sketchSize / (divNum * divNum)) * divNum);
+
     do {
         counter++;
         let newDiv = document.createElement("div");
         let divStyle = newDiv.style;
         divStyle.height = `${divSize}px`;
         divStyle.width = `${divSize}px`;
-        containerBox.appendChild(newDiv);
-    } while(counter < divNum);
+        sketchBox.appendChild(newDiv);
+    } while(counter < (divNum * divNum));
 }
 
-function calcSize(divNum) {
-    divSize = viewWidth / divNum;
-}
-
-addDivs(16);
+addDivs(15);
